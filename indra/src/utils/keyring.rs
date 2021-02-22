069f8e1ac010045b764a2776cdd3252d99a46017
laynath242@gmail.com <laynath242@gmail.com>
2021-02-22 13:32:58 +0700

    indracore keyring

--- indra/src/utils/keyring.rs
+++ indra/src/utils/keyring.rs
@@ -1,46 +1,52 @@
-use crate::models::crypto::KeyPair;
 use std::str::FromStr;
-use subxt::{
+use substrate_subxt::{
     sp_core::{crypto, sr25519, Pair as TraitPair},
     sp_runtime, Error, IndracoreRuntime, PairSigner,
 };
 
-pub fn from_string_to_accountid(account_id: &str) -> Result<crypto::AccountId32, Error> {
-    match sp_runtime::AccountId32::from_str(account_id) {
-        Ok(id) => Ok(id),
-        Err(e) => return Err(Error::Other(e.into())),
-    }
+type KusamaPairSigner = PairSigner<IndracoreRuntime, sr25519::Pair>;
+type MultiAddressId = sp_runtime::MultiAddress<crypto::AccountId32, ()>;
+
+#[derive(Clone)]
+pub struct IndraKeyring {
+    pairsigner: KusamaPairSigner,
+    accountid: crypto::AccountId32,
 }
 
-pub fn get_keypair(phrase: &str) -> Result<KeyPair, Error> {
-    match sr25519::Pair::from_string(&phrase, None) {
-        Ok(p) => {
-            let signer = PairSigner::<IndracoreRuntime, sr25519::Pair>::new(p.clone());
-            let account_id = crypto::AccountId32::from(p.clone().public());
-            Ok(KeyPair { signer, account_id })
+impl IndraKeyring {
+    pub fn from_mnemonic(mnemonic: &'static str) -> Result<IndraKeyring, Error> {
+        match sr25519::Pair::from_string(&mnemonic, None) {
+            Ok(pair) => {
+                let pairsigner = PairSigner::<IndracoreRuntime, sr25519::Pair>::new(pair.clone());
+                let accountid = crypto::AccountId32::from(pair.clone().public());
+                Ok(Self {
+                    pairsigner,
+                    accountid,
+                })
+            }
+            Err(e) => Err(Error::Other(format!("{:?}", e))),
         }
-        Err(e) => Err(Error::Other(format!("{:?}", e)))
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn test_get_accountid_from_string() {
-        let alice = "BauKu2iL4fncgfy22YSLGc1aDLpyuUUe5z8yNF2pDtLNr4E";
-        let bob = "A1k3praCLftTgBTb6aVavh3UNKwXN599Fqov17MkEy6bwCU";
-        let alice_id = from_string_to_accountid(alice);
-        let bob_id = from_string_to_accountid(bob);
-        assert_ne!(alice_id.unwrap(), bob_id.unwrap());
     }
 
-    #[test]
-    fn test_keypair() {
-        let phrase = "elbow crop stairs spy book stable merry lumber elite uncle evil shadow";
-        let keypair = get_keypair(phrase).unwrap();
+    pub fn pairsingner(&self) -> KusamaPairSigner {
+        self.pairsigner.clone()
+    }
 
-        let test_id = from_string_to_accountid("8ZkP4bcdb9yHZLRKH1soMSg19aHpuURAtuaAZETP6JHt7HU").unwrap();
-        assert_eq!(keypair.account_id, test_id);
+    pub fn accountid(&self) -> crypto::AccountId32 {
+        self.accountid.clone()
+    }
+
+    pub fn accountid_from_str(accountid: &'static str) -> Result<crypto::AccountId32, Error> {
+        match sp_runtime::AccountId32::from_str(accountid) {
+            Ok(id) => Ok(id),
+            Err(e) => return Err(Error::Other(e.into())),
+        }
+    }
+
+    pub fn multi_address_from_str(accountid: &'static str) ->Result<MultiAddressId, Error>  {
+        match sp_runtime::AccountId32::from_str(accountid) {
+            Ok(id) => Ok(sp_runtime::MultiAddress::from(id)),
+            Err(e) => return Err(Error::Other(e.into())),
+        }
     }
 }
